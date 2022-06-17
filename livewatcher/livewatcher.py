138df8e0d30252154ad9020acf324f4b3f6276a2
voidbean <41186092+voidbean@users.noreply.github.com>
2022-06-17 17:51:42 +0800

    fix: -412

--- livewatcher/livewatcher.py
+++ livewatcher/livewatcher.py
@@ -6,7 +6,7 @@ import time
 #Server_chan的key
 SCKEY = '此处填写您在 sc.ftqq.com 申请到的SCKEY'
 
-bili_url = "http://api.bilibili.com/x/space/acc/info"
+bili_url = "https://api.bilibili.com/x/space/app/index"
 server_chan_url = f"https://sc.ftqq.com/{SCKEY}.send"
 
 #数据字典的key为主播id, value为true或false均可
@@ -36,15 +36,15 @@ async def send(title, message):
 async def main():
     for uid in live_cache:
         resp_json = await get_status(uid)
-        status = resp_json['data']['live_room']['liveStatus']
+        status = resp_json['data']['info']['live']['liveStatus']
         if live_cache[uid] == 'true':
             if status == 0:
                 live_cache[uid] = 'false'
         else:
             if status == 1:
                 live_cache[uid] = 'true'
-                title = f"您关注的{resp_json['data']['name']}开播了!"
-                message = f"直播标题:{resp_json['data']['live_room']['title']}\n{resp_json['data']['live_room']['url']}"
+                title = f"您关注的{resp_json['data']['info']['name']}开播了!"
+                message = f"直播标题:{resp_json['data']['info']['live']['title']}\n{resp_json['data']['info']['live']['url']}"
                 await send(title, message)
 
 
